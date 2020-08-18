ff75048d65c868171909b76f90bdaca126b09015
joselages <jose.lages.d12@gmail.com>
2020-08-18 10:31:06 +0100

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,51 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="container">
+      <span id="start" class="start"></span>
+      <span id="end" class="end"></span>
+      <img src="./assets/mapa.png" />
+      <img class="train" src="./assets/train.svg" />
+    </div>
+    <button @click.prevent="getJourney" class="js-btn">OLA</button>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import LeaderLine from "leader-line-vue";
+const comboios = require("comboios");
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  mounted() {
+    LeaderLine.setLine(
+      document.getElementById("start"),
+      document.getElementById("end"),
+      {
+        color: "#ef1e28",
+        startPlug: "square",
+        endPlug: "square",
+        startPlugColor: "transparent",
+        endPlugColor: "transparent",
+        startPlugSize: 1.5,
+        endPlugSize: 1.5,
+        dash: true,
+        path: "straight",
+      }
+    );
+  },
+  methods: {
+    getJourney: function () {
+      const porto = "94-2006";
+      const lisboa = {
+        type: "station",
+        id: "94-30007",
+        name: "Lisboa - Santa Apolonia",
+      };
+
+      comboios.journeys(porto, lisboa).then(console.log).catch(console.error);
+    },
+  },
+};
 </script>
 
 <style>
@@ -24,5 +56,50 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+body {
+  display: flex;
+  align-items: center;
+}
+.container {
+  position: relative;
+}
+
+.start,
+.end {
+  position: absolute;
+}
+
+.start {
+  top: 75px;
+  left: 200px;
+}
+
+.end {
+  top: 700px;
+  left: 75px;
+}
+
+img {
+  width: 600px;
+}
+
+.train {
+  width: 45px;
+  top: 0;
+  left: 0;
+  position: absolute;
+  /* transform: rotate(-180deg); */
+  offset-rotate: reverse;
+  offset-path: path("M 200 75 L 75 700");
+  offset-distance: 00%;
+}
+
+.js-btn {
+  background-color: rebeccapurple;
+  color: white;
+  font-weight: bold;
+  padding: 1rem;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,29 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
+/* import LoadScript from "vue-plugin-load-script";
 
-Vue.config.productionTip = false
+Vue.use(LoadScript);
+Vue.loadScript("leader-line.min.js"); */
+
+Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render: (h) => h(App),
+  /*   mounted() {
+    new LeaderLine(
+      document.getElementById("start"),
+      document.getElementById("end"),
+      {
+        color: "#ef1e28",
+        startPlug: "square",
+        endPlug: "square",
+        startPlugColor: "transparent",
+        endPlugColor: "transparent",
+        startPlugSize: 1.5,
+        endPlugSize: 1.5,
+        dash: true,
+        path: "straight",
+      }
+    );
+  }, */
+}).$mount("#app");
